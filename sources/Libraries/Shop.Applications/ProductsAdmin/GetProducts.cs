21cb17bbb0f3b794d77d0ab5f95477b1140c4fc1
Rabbi <rabbycse7@gmail.com>
2019-08-29 13:46:55 +0600

    Create Product Admin Service

--- sources/Libraries/Shop.Applications/ProductsAdmin/GetProducts.cs
+++ sources/Libraries/Shop.Applications/ProductsAdmin/GetProducts.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shop.Applications.Products 
+namespace Shop.Applications.ProductsAdmin 
 {
     using Database;
-    public class GetProducts 
+    public class GetProducts
     {
         private ApplicationDbContext _context;
 
@@ -18,18 +18,18 @@ namespace Shop.Applications.Products
 
         public IEnumerable<ProductViewModel> Do() =>
          _context.Products.ToList().Select(x => new ProductViewModel
-        {
-            Id = x.Id,
-            Name = x.Name,
-            Description = x.Description,
-            Value = $"£ {x.Value.ToString("N2")}", //1100.0 => 1,100.50 => £ 1,100.50
+         {
+             Id = x.Id,
+             Name = x.Name,
+             Description = x.Description,
+             Value = x.Value,
          });
         public class ProductViewModel
         {
             public int Id { get; set; }
             public string Name { get; set; }
             public string Description { get; set; }
-            public string Value { get; set; }
+            public decimal Value { get; set; }
         }
     }
 
---
+++ sources/Libraries/Shop.Applications/ProductsAdmin/UpdateProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Applications.ProductsAdmin 
+{
+    using Shop.Database;
+    using Shop.Domain.Models;
+    public class UpdateProduct
+    { 
+        private ApplicationDbContext _context;
+
+        public UpdateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Do(ProductViewModel vm)
+        {
+            await _context.SaveChangesAsync(); 
+        }
+        public class ProductViewModel
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
+        
+    }
+
+}
